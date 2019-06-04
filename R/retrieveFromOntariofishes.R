707e7dd1112c22e101298d7659b35611c253079b
Kevin Cazelles <kcazelle@uoguelph.ca>
2019-06-04 17:43:37 -0400

    add itis classification info

--- R/retrieveFromOntariofishes.R
+++ R/retrieveFromOntariofishes.R
@@ -1,5 +1,7 @@
 library(tidyverse)
+library(taxize)
 
+## First - simple webscrapping
 ls_data <- list()
 for (i in 1:159) {
   print(i)
@@ -16,6 +18,60 @@ for (i in 1:159) {
 df <- ls_data %>% lapply(function(x) x[,2]) %>% do.call(rbind, .) %>%
   as.data.frame
 
-names(df) <- ls_data[[1]][,1]
+## Columns names
+names(df) <- gsub(ls_data[[1]][,1], pattern = " ", replacement = "_") %>%
+  gsub(pattern = "\\(", replacement = "") %>%
+  gsub(pattern = "\\)", replacement = "") %>%
+  gsub(pattern = "&deg;C", replacement = "Celsius")
 
-write.csv(df, "output/ontariofishes.csv")
+## I only use the common term for families and retrieve the classification
+## from taxize
+df$Family <- strsplit(df$Family, " - ") %>% lapply(function(x) x[2]) %>% unlist
+names(df)
+
+## retrive classification form itis
+res <- list()
+df0 <- data.frame(
+  class = NA, order = NA, family = NA, genus = NA, species = NA
+)
+## A couple of tsn need to be updated (valid names but wrong tsn...)
+## Catostomus commersonii; tsn => 553273
+## Chrosomus eos ; tsn => 913993
+## Chrosomus neogaeus ; tsn => 913995
+## Coregonus artedi;  tsn => 623384
+## Coregonus hoyi => 623394
+## Coregonus nigripinnis => 623395
+## Coregonus nipigon => Coregonus artedi; tsn => 623384
+## Labidesthes sicculus; tsn => 166016
+## Lethenteron appendix; tsn => 914061
+## Lepomis gulosus; tsn => 168138 (invalid=>Chaenobryttus gulosis)
+## Moxostoma duquesnei (name invalid) => Moxostoma duquesnii; tsn => 553274
+## Prosopium coulterii; tsn => 553389
+df$Species_TSN2 <- df$Species_TSN
+
+wsp <- function(x) unlist(lapply(x, function(y) which(df$Species == y)))
+
+nm <- c("Catostomus commersonii", "Chrosomus eos", "Chrosomus neogaeus",
+"Coregonus artedi", "Coregonus hoyi",  "Coregonus nigripinnis", "Coregonus nipigon",
+"Labidesthes sicculus", "Lethenteron appendix", "Lepomis gulosus",
+"Moxostoma duquesnei", "Prosopium coulterii")
+
+tsn <- c(553273, 913993, 913995, 623384, 623394, 623395, 623384, 166016, 914061, 168138, 553274, 553389)
+df$Species_TSN2[wsp(nm)] <- tsn
+
+nm <- c("class", "order", "family", "genus", "species")
+for (i in 1:nrow(df)) {
+  print(i)
+  res[[i]] <- df0
+  if (df$Species_TSN2[i] != "not applicable") {
+    tmp <- classification(df$Species_TSN2[i], db = "itis")
+    id <- nm %m% tmp[[1]]$rank
+    res[[i]][1,] <- tmp[[1]]$name[id]
+  }
+}
+##
+info_itis <- do.call(rbind, res)
+names(info_itis) <- paste0(names(info_itis), "_itis")
+out <- cbind(df, info_itis)
+
+write.csv(df, "output/ontariofishes_raw.csv")
--- README.md
+++ README.md
@@ -1,8 +1,14 @@
 # fishCharactON
 
-Characteristics of Ontario fish species
+Characteristics of Ontario fish species retrieved from http://www.ontariofishes.ca.
 
 
 ## References
 
-http://www.ontariofishes.ca/fish_detail.php?FID=110
+Cite Ontario Freshwater Fishes Life History Database as:
+
+Eakins, R. J. 2018. Ontario Freshwater Fishes Life History Database. Version 4.84. Online database. (http://www.ontariofishes.ca), accessed 09 April 2019
+
+## Todo
+
+Use taxize to retrive taxo data.
